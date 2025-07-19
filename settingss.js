11b7ebc19a89e6126d11c56d9fe39ca3ece86094
rohaid-ops <ranarohaid962@gmail.com>
2025-07-19 21:10:42 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "ALI-MD~6N1xECwR#9GqvmxhdWCxotHuzuEKHQ_O04e1NrNRA4SAWflJdMj0 ",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
